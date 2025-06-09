1185a20a715ff2efc3b20bd845e9c571083a7b01
supremalexx <88965302+supremalexx@users.noreply.github.com>
2025-06-09 21:52:33 +0200

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,3 +1,5 @@
+console.log("SUPABASE_URL", process.env.SUPABASE_URL)
+console.log("SUPABASE_ANON_KEY", process.env.SUPABASE_ANON_KEY)
 import { createServerClient, type CookieOptions } from "@supabase/ssr"
 import { cookies } from "next/headers"
 
