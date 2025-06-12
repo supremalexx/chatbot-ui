bec4cac12c9f8a0a5390ab93f017aa469ac1d9a4
supremalexx <88965302+supremalexx@users.noreply.github.com>
2025-06-12 03:13:31 +0200

    Update page.tsx

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -34,17 +34,17 @@ export default async function Login({
   const session = (await supabase.auth.getSession()).data.session
 
   if (session) {
-    const { data: homeWorkspace, error } = await supabase
+    const { data: workspaces, error } = await supabase
       .from("workspaces")
       .select("*")
       .eq("user_id", session.user.id)
       .eq("is_home", true)
-      .single()
 
-    if (!homeWorkspace) {
-      throw new Error(error.message)
+    if (error || !workspaces || workspaces.length !== 1) {
+      return redirect("/setup")
     }
 
+    const homeWorkspace = workspaces[0]
     return redirect(`/${homeWorkspace.id}/chat`)
   }
 
@@ -65,19 +65,17 @@ export default async function Login({
       return redirect(`/login?message=${error.message}`)
     }
 
-    const { data: homeWorkspace, error: homeWorkspaceError } = await supabase
+    const { data: workspaces, error: homeWorkspaceError } = await supabase
       .from("workspaces")
       .select("*")
       .eq("user_id", data.user.id)
       .eq("is_home", true)
-      .single()
 
-    if (!homeWorkspace) {
-      throw new Error(
-        homeWorkspaceError?.message || "An unexpected error occurred"
-      )
+    if (homeWorkspaceError || !workspaces || workspaces.length !== 1) {
+      return redirect("/setup")
     }
 
+    const homeWorkspace = workspaces[0]
     return redirect(`/${homeWorkspace.id}/chat`)
   }
 
@@ -86,7 +84,6 @@ export default async function Login({
     if (process.env.EDGE_CONFIG) {
       return await get<string>(name)
     }
-
     return process.env[name]
   }
 
@@ -96,27 +93,21 @@ export default async function Login({
     const email = formData.get("email") as string
     const password = formData.get("password") as string
 
-    const emailDomainWhitelistPatternsString = await getEnvVarOrEdgeConfigValue(
-      "EMAIL_DOMAIN_WHITELIST"
-    )
+    const emailDomainWhitelistPatternsString = await getEnvVarOrEdgeConfigValue("EMAIL_DOMAIN_WHITELIST")
     const emailDomainWhitelist = emailDomainWhitelistPatternsString?.trim()
       ? emailDomainWhitelistPatternsString?.split(",")
       : []
-    const emailWhitelistPatternsString =
-      await getEnvVarOrEdgeConfigValue("EMAIL_WHITELIST")
+
+    const emailWhitelistPatternsString = await getEnvVarOrEdgeConfigValue("EMAIL_WHITELIST")
     const emailWhitelist = emailWhitelistPatternsString?.trim()
       ? emailWhitelistPatternsString?.split(",")
       : []
 
-    // If there are whitelist patterns, check if the email is allowed to sign up
-    if (emailDomainWhitelist.length > 0 || emailWhitelist.length > 0) {
-      const domainMatch = emailDomainWhitelist?.includes(email.split("@")[1])
-      const emailMatch = emailWhitelist?.includes(email)
-      if (!domainMatch && !emailMatch) {
-        return redirect(
-          `/login?message=Email ${email} is not allowed to sign up.`
-        )
-      }
+    const domainMatch = emailDomainWhitelist.includes(email.split("@")[1])
+    const emailMatch = emailWhitelist.includes(email)
+
+    if ((emailDomainWhitelist.length || emailWhitelist.length) && !domainMatch && !emailMatch) {
+      return redirect(`/login?message=Email ${email} is not allowed to sign up.`)
     }
 
     const cookieStore = cookies()
@@ -125,10 +116,6 @@ export default async function Login({
     const { error } = await supabase.auth.signUp({
       email,
       password,
-      options: {
-        // USE IF YOU WANT TO SEND EMAIL VERIFICATION, ALSO CHANGE TOML FILE
-        // emailRedirectTo: `${origin}/auth/callback`
-      }
     })
 
     if (error) {
@@ -137,9 +124,6 @@ export default async function Login({
     }
 
     return redirect("/setup")
-
-    // USE IF YOU WANT TO SEND EMAIL VERIFICATION, ALSO CHANGE TOML FILE
-    // return redirect("/login?message=Check email to continue sign in process")
   }
 
   const handleResetPassword = async (formData: FormData) => {
@@ -169,51 +153,22 @@ export default async function Login({
       >
         <Brand />
 
-        <Label className="text-md mt-4" htmlFor="email">
-          Email
-        </Label>
-        <Input
-          className="mb-3 rounded-md border bg-inherit px-4 py-2"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
+        <Label className="text-md mt-4" htmlFor="email">Email</Label>
+        <Input className="mb-3 rounded-md border bg-inherit px-4 py-2" name="email" placeholder="you@example.com" required />
 
-        <Label className="text-md" htmlFor="password">
-          Password
-        </Label>
-        <Input
-          className="mb-6 rounded-md border bg-inherit px-4 py-2"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-        />
+        <Label className="text-md" htmlFor="password">Password</Label>
+        <Input className="mb-6 rounded-md border bg-inherit px-4 py-2" type="password" name="password" placeholder="••••••••" />
 
-        <SubmitButton className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white">
-          Login
-        </SubmitButton>
-
-        <SubmitButton
-          formAction={signUp}
-          className="border-foreground/20 mb-2 rounded-md border px-4 py-2"
-        >
-          Sign Up
-        </SubmitButton>
+        <SubmitButton className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white">Login</SubmitButton>
+        <SubmitButton formAction={signUp} className="border-foreground/20 mb-2 rounded-md border px-4 py-2">Sign Up</SubmitButton>
 
         <div className="text-muted-foreground mt-1 flex justify-center text-sm">
           <span className="mr-1">Forgot your password?</span>
-          <button
-            formAction={handleResetPassword}
-            className="text-primary ml-1 underline hover:opacity-80"
-          >
-            Reset
-          </button>
+          <button formAction={handleResetPassword} className="text-primary ml-1 underline hover:opacity-80">Reset</button>
         </div>
 
         {searchParams?.message && (
-          <p className="bg-foreground/10 text-foreground mt-4 p-4 text-center">
-            {searchParams.message}
-          </p>
+          <p className="bg-foreground/10 text-foreground mt-4 p-4 text-center">{searchParams.message}</p>
         )}
       </form>
     </div>
